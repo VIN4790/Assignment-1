6ae6aaeec43280c7fe75fcdcfc59a4b9f4ac3b38
VIN4790 <134125166+VIN4790@users.noreply.github.com>
2023-06-01 11:36:06 +0530

    Update Assignment-1.py

--- Assignment-1.py
+++ Assignment-1.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
-
-
-
-
+# 1.TWO SUM PROBLEM
+def chekpair(A,size,x):
+    for i in range(0,size-1):
+        for j in range(1,size):
+           if A[i]+A[j]==x:
+                return([i,j])
+            
+A=[1,3,15,6,5]
+size=len(A)
+x=7
+print(chekpair(A,size,x))
 
 # In[ ]:
 
